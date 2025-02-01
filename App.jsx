ec39daf16595e46c2001cd788365a71615e7080e
sithul-developer <sothsithul99@gmail.com>
2025-02-01 23:40:35 +0700

    initial

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+const App = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default App;
